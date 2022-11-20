187b8dbf96a816d84987ac1d05263981e7fc8c50
Vladimir <reurk@gmail.com>
2022-11-20 11:54:03 +0300

    remove dep

--- deploy.bat
+++
@@ -1,8 +0,0 @@
-  ECHO ON
-  cd /d "%~dp0
-  set deployPath=..\deploy
-  echo %deployPath%
-  set backPath=..\back\SespelInventory\cmd\
-    echo %backPath%
-  xcopy "./build" %deployPath%\public\ /s /e /h
-  xcopy "./build" %backPath%\public\ /s /e /h
