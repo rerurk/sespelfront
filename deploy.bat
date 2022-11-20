2ca8e6fb402965aa9cf5f6d576ffe73495c2ae32
Vladimir <reurk@gmail.com>
2022-11-20 11:52:53 +0300

    modify deply

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,7 @@
   cd /d "%~dp0
   set deployPath=..\deploy
   echo %deployPath%
-  set backPath=C:\Progects\sesepelInventory\back\SespelInventory\cmd\
+  set backPath=..\back\SespelInventory\cmd\
     echo %backPath%
   xcopy "./build" %deployPath%\public\ /s /e /h
   xcopy "./build" %backPath%\public\ /s /e /h
