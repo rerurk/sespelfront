a2c611f32499d1e1443119924031bda79c198d57
rerurk <rerurk@gmail.com>
2022-12-05 14:51:52 +0300

    c0512221451

--- src/components/UI/cahngeFontSize/ChangeFontSize.tsx
+++ src/components/UI/cahngeFontSize/ChangeFontSize.tsx
@@ -1,27 +1,54 @@
 import React, {FC, useEffect} from 'react';
 // @ts-ignore
 import cl from "./ChangeFontSize.module.css"
-const step:number=5
+import {Tools} from "../../../tools/Tools";
+
+const step: number = 2
 
 
-const ChangeFontSize:FC = () => {
+const ChangeFontSize: FC = () => {
 
-    let def:number=60
-    let maxS:number=Math.max(window.innerHeight,window.innerWidth)
-    useEffect(()=>{
-        change(0)
-    })
-    function change(n:number) {
-         def=def+n
+    let def: number = 60
+    let maxS: number = Math.max(window.innerHeight, window.innerWidth)
+    useEffect(() => {
+        let tempN:number|null=loadFontSize()
+    
+        if (tempN){
+            def=tempN
+            change(0)
+        }else{
+            saveFrontSize(def)
+        }
+        return saveFrontSize(def)
+    },[])
+
+    function saveFrontSize(n: number) {
+        localStorage.setItem("fontSize", String(n))
+    }
+
+    function loadFontSize(): number | null {
+        let nstr: string | null = localStorage.getItem("fontSize")
+        if (nstr) {
+            let n: number = Number(nstr)
+            return n
+        }
+        return null
+
+    }
+
+    function change(n: number) {
+        def = def + n
+
 
         let h = document.getElementsByTagName('html')[0]
-
-        h.style.fontSize = Math.floor(maxS/ (def)) + 'px'
+        h.style.fontSize = Math.floor(maxS / (def)) + 'px'
+        saveFrontSize(def)
     }
+
     return (
         <div className={cl.wrapper}>
-            <button onClick={()=>change(+step)}>-</button>
-            <button onClick={()=>change(-step)}>+</button>
+            <button onClick={() => change(+step)}>-</button>
+            <button onClick={() => change(-step)}>+</button>
 
         </div>
     );
--- src/tools/Tools.ts
+++ src/tools/Tools.ts
@@ -1,13 +1,13 @@
 import {CatalogItem} from "../structs/catalog";
 
 
-type CatalogItemLS={
-    isOpen:boolean|undefined
-    date:string
+type CatalogItemLS = {
+    isOpen: boolean | undefined
+    date: string
 }
 
 export class Tools {
-    public static unRefCatalogItem(it:CatalogItem):CatalogItem{
+    public static unRefCatalogItem(it: CatalogItem): CatalogItem {
         return {
             id: it.id,
             items: null,
@@ -19,33 +19,47 @@ export class Tools {
 
         }
     }
-    public static SaveCatalogItemFields(catalogItem:CatalogItem){
 
-               let catItLS:CatalogItemLS={
-                   isOpen:catalogItem.isOpen,
-                   date:Date.now().toString()
-               }
-               localStorage.setItem(catalogItem.ref,JSON.stringify(catItLS))
+    public static SaveCatalogItemFields(catalogItem: CatalogItem) {
+
+        let catItLS: CatalogItemLS = {
+            isOpen: catalogItem.isOpen,
+            date: Date.now().toString()
+        }
+        localStorage.setItem(catalogItem.ref, JSON.stringify(catItLS))
 
 
     }
-    public static LoadCatalogItemFields(catalogItem:CatalogItem){
-        let str:string | null
 
-        str=localStorage.getItem(catalogItem.ref)
+    public static LoadCatalogItemFields(catalogItem: CatalogItem) {
+        let str: string | null
 
-        let catItLS:CatalogItemLS
-        if(str){
+        str = localStorage.getItem(catalogItem.ref)
+
+        let catItLS: CatalogItemLS
+        if (str) {
             try {
-                catItLS=JSON.parse(str)
-                catalogItem.isOpen=catItLS.isOpen
-            }catch (e) {
-                catalogItem.isOpen=false
+                catItLS = JSON.parse(str)
+                catalogItem.isOpen = catItLS.isOpen
+            } catch (e) {
+                catalogItem.isOpen = false
             }
 
         }
 
 
+    }
+
+    public static SaveFrontSize(n: number) {
+        localStorage.setItem("fontSize", String(n))
+    }
+
+    public static LoadFontSize(): number|null {
+        let nstr: string | null = localStorage.getItem("fontSize")
+        if (nstr) {
+            let n: number = Number(nstr)
+        }
+        return null
 
     }
 
