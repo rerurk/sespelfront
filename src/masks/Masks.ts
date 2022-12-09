46cbbb56c1e4047445dece4c312ed047081ac2a4
rerurk <rerurk@gmail.com>
2022-12-09 15:41:24 +0300

    c0912221541

--- src/masks/Masks.ts
+++ src/masks/Masks.ts
@@ -1,7 +1,8 @@
+
+
 export enum Masks {
-    NOT_ACTUAL_MASK = 1 << 31,
-    CATALOG_MASK = 1 << 30,
-    CATALOG_ITEM_MASK = 1 << 29,
-    NON_REMOVABLE = 1 << 28,
-    STORAGE_MASK = 1 << 27,
+    CATALOG_MASK = 1 << 0,
+    CATALOG_ITEM_MASK=1 << 1,
+    NON_REMOVABLE=1 << 2 ,
+    STORAGE_MASK=1 << 3
 }
