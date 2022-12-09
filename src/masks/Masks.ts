7d5d0c5620fd8ff5c627afe21ef7e287bc08b4ba
Vladimir <reurk@gmail.com>
2022-12-09 07:47:42 +0300

    c091222747

--- src/masks/Masks.ts
+++ src/masks/Masks.ts
@@ -1,6 +1,7 @@
 export enum Masks {
+    NOT_ACTUAL_MASK = 1 << 31,
     CATALOG_MASK = 1 << 30,
     CATALOG_ITEM_MASK = 1 << 29,
     NON_REMOVABLE = 1 << 28,
-    STORAGE_MASK = 1 << 27
+    STORAGE_MASK = 1 << 27,
 }
