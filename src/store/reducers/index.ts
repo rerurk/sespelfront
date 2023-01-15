fcfabc01c5acd08709c6187fb5db67325fcfc2ae
rerurk <76093904+rerurk@users.noreply.github.com>
2023-01-15 15:26:52 +0300

    FIERST

--- src/store/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux";
-import {appReducer} from "./appReducer";
-
-
-export const rootReducer =combineReducers({
- appReducer:appReducer
-
-})
-export type RootState = ReturnType<typeof rootReducer>
--- src/structs/storesTypes.ts
+++
@@ -1,7 +0,0 @@
-import {Item} from "./App";
-import {NomenclItemAndHisUUIDS} from "./Asset";
-
-export type StoreBalance = {
-    store:Item
-    nomenclItemAndHisUUIDS:NomenclItemAndHisUUIDS[]
-}
